cdd5661b914e671aa2611797b574a97e9ebd8905
Tasmeer Naeem <Samk.code@gmail.com>
2023-11-21 05:13:19 +0500

    changes

--- src/components/DisplayData.vue
+++ src/components/DisplayData.vue
@@ -15,6 +15,15 @@
 import barChart from './charts/barChart.vue'
 import lineChart from './charts/lineChart.vue'
 import {  ref } from 'vue';
+import {ref,onMounted} from 'vue'
+import { useMyStore } from '../store/store';
+const barchartData = ref(null)
+onMounted(async () => {
+  const myStore = useMyStore();
+    await myStore.fetchDataset();
+  
+    barchartData.value = myStore.getDataset
+})
 
 const showChart = ref(false)
 const barChartBtn = ref('Show Bar Chart')
